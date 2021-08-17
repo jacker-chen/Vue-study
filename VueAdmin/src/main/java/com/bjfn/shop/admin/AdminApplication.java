005257b3ae6835f41898679e9d4becfc01835ffd
bjfn <534246362@qq.com>
2021-08-17 15:49:31 +0800

    升级到springcloud并整合nacos

--- VueAdmin/src/main/java/com/bjfn/shop/admin/AdminApplication.java
+++ VueAdmin/src/main/java/com/bjfn/shop/admin/AdminApplication.java
@@ -3,9 +3,11 @@ package com.bjfn.shop.admin;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan("com.bjfn.shop.admin.mapper")
+@EnableDiscoveryClient
 public class AdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminApplication.class,args);
